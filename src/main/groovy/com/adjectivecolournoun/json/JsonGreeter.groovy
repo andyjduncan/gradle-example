c62d6e6ab19853f56190bb7f6b10f73a0ae9dba3
kishore3github <94594723+kishore3github@users.noreply.github.com>
2024-11-05 07:51:38 +0530

    Update JsonGreeter.groovy

--- src/main/groovy/com/adjectivecolournoun/json/JsonGreeter.groovy
+++ src/main/groovy/com/adjectivecolournoun/json/JsonGreeter.groovy
@@ -11,13 +11,3 @@ class JsonGreeter {
     }
 }
 
-public class HardcodedPasswordExample {
-    public void authenticate() {
-        String password = "hardcodedPassword123"; // Vulnerable line
-        if (password.equals("expectedPassword")) {
-            System.out.println("Authenticated!");
-        } else {
-            System.out.println("Authentication failed!");
-        }
-    }
-}
